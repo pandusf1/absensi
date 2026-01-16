647852b4645a144f18910907232b9c5ad857cdc1
pandusf1 <pandusf1@gmail.com>
2026-01-17 00:38:23 +0700

    Asda

--- api/database.php
+++ api/database.php
@@ -1,14 +1,17 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "absensi";
+$host = "gateway01.ap-southeast-1.prod.aws.tidbcloud.com";
+$user = "3UEpyYnuidk1vBW.root";
+$pass = "OvCZ7LN1zbK6Njq2";
+$db   = "test";
+$port = 4000;
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL); 
+mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL);
 
-if ($conn->connect_error) {
-    die("Koneksi database gagal: " . $conn->connect_error);
+if (mysqli_connect_errno()) {
+    die("Gagal Konek Database Cloud: " . mysqli_connect_error());
 }
 ?>
