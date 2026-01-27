b3ba2d6ac347f2c99d25184a461cdb5cd88e5880
pandusf1 <pandusf1@gmail.com>
2026-01-27 17:22:59 +0700

    done

--- api/mahasiswa/mahasiswa_dash.php
+++ api/mahasiswa/mahasiswa_dash.php
@@ -400,6 +400,7 @@ $page = isset($_GET['page']) ? $_GET['page'] : 'home';
                 <p id="msgReg" style="margin-top:15px; font-weight:600; color:#3b82f6;"></p>
             </div>
         <?php endif; ?>
+        <footer style="text-align: center;">© 2025 Penelitian Terapan Pratama</footer>
     </div>
 <script>
         // 1. Variabel Global
